48a732ca7b7a616f89f4201d978741dbb1af58b1
Jebran Shafiq <jebranzxc@gmail.com>
2026-08-13 18:06:31 +0530

    Time: 4 ms (21.28%), Space: 77.3 MB (13.16%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,55 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* t1 = l1;
-        ListNode* t2 = l2;
-
-        vector<int> s1;
-        vector<int> s2;
-
-        while (t1 != NULL) {
-            s1.push_back(t1->val);
-            t1 = t1->next;
-        }
-        while (t2 != NULL) {
-            s2.push_back(t2->val);
-            t2 = t2->next;
-        }
-        vector<int> result;
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
 
         int carry = 0;
-        int n = max(s1.size(), s2.size());
 
-        for (int i = 0; i < n; i++) {
-            int a = (i < s1.size()) ? s1[i] : 0;
-            int b = (i < s2.size()) ? s2[i] : 0;
+        while (l1 != NULL || l2 != NULL || carry) {
+            int a = (l1 != NULL) ? l1->val : 0;
+            int b = (l2 != NULL) ? l2->val : 0;
 
             int sum = a + b + carry;
 
-            result.push_back(sum % 10);
             carry = sum / 10;
+
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+
+            if (l1 != NULL)
+                l1 = l1->next;
+
+            if (l2 != NULL)
+                l2 = l2->next;
         }
 
-        if (carry)
-            result.push_back(carry);
-
-        ListNode* head = new ListNode(result[0]);
-        ListNode* prev = head;
-        for (int i = 1; i < result.size(); i++) {
-            ListNode* temp = new ListNode(result[i], nullptr);
-            prev->next = temp;
-            prev = temp;
-        }
-        return head;
+        return dummy->next;
     }
 };
