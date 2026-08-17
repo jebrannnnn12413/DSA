89b36abf5bc4210ad7e48b812ad1005b16de71bd
Jebran Shafiq <jebranzxc@gmail.com>
2026-08-17 21:30:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.48%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
-        
+        long long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1;
+
+        for (; N > 0; N /= 2) {
+            if (N % 2 == 1) {
+                ans *= x;
+            }
+
+            x *= x;
+        }
+
+        return ans;
     }
 };
