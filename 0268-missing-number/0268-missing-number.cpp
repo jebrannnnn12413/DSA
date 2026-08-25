97ff7f1674da61f55334a0f679330fc29574de1d
Jebran Shafiq <jebranzxc@gmail.com>
2026-08-26 00:32:02 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.69%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1=0;
+        int xor2=0;
+
+        for(int i=0;i<=nums.size();i++){
+            xor1^=i;
+        }
+        for(int i=0;i<nums.size();i++){
+            xor2^=nums[i];
+        }
+
+        return xor1^xor2;
+
+        
+    }
+};
