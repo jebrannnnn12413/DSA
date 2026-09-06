fa0dab2abde25d2026a965cd899dffe3160420b4
Jebran Shafiq <jebranzxc@gmail.com>
2026-09-06 16:31:58 +0530

    Time: 3 ms (27.83%), Space: 23.1 MB (5.99%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& arr) {
+        list<int> st;
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] > 0) {
+                st.push_back(arr[i]);
+            } else {
+                while (!st.empty() && st.back() > 0 &&
+                       st.back() < abs(arr[i])) {
+                    st.pop_back();
+                }
+                if (!st.empty() && st.back() == abs(arr[i])) {
+                    st.pop_back();
+                } else if (st.empty() || st.back() < 0) {
+                    st.push_back(arr[i]);
+                }
+            }
+        }
+        return vector<int>(st.begin(), st.end());
+        ;
+    }
+};
